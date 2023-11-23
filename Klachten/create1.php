9f59cb127de77421116eb22a1547c30b795ee05e
Adonaï <9014497@student.zadkine.nl>
2023-11-23 10:50:35 +0100

    heb een create gemaakt

---
+++ Klachten/create1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../stylesheet.css">
+</head>
+<body>
+
+<header>
+
+    <div class="navbar">
+        <img src="Images/Logo-gemeente-Rotterdam.png" alt="Rotterdam_logo">
+    </div>
+
+</header>
+
+<form action="create2.php" method="post">
+    <label for "Wat is uw klacht?">Wat is uw klacht? </label>
+    <input type="text" name="Wat is uw klacht"></input>
+    <br/>
+    <label for "Beschrijving">Beschrijving</label>
+    <input type="text" name="Beschrijving"></form>
+</input>
+<br/>
+<a href="create2.php"><input type="submit"></a>
+</form>
+</body>
+</html>
+
---
+++ Klachten/create2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../stylesheet.css">
+</head>
+<body>
+
+<header>
+
+    <div class="navbar">
+        <img src="Images/Logo-gemeente-Rotterdam.png" alt="Rotterdam_logo">
+    </div>
+
+</header>
+<?php
+require "klachten.php";
+// uitlezen vakjes van create1 ------
+$Soort=$_POST["Soort"];
+$Beschrijving=$_POST["Beschrijving"];
+
+// maken object --------------------------------------------
+$klachten1 = new klachten($Soort, $Beschrijving);
+$klachten1->create();
+
+// afdrukken object ------------------------------
+echo "<br/>";
+echo ($Soort);
+echo "<br/>";
+echo ($Beschrijving);
+?>
+</body>
+</html>
+
