15f13f03642e27f08424891ea28703ce33922e0b
Adonaï <9014497@student.zadkine.nl>
2023-12-05 13:41:04 +0100

    locatie toegevoegd

--- locatie/map.php
+++ locatie/map.php
@@ -1,68 +1,40 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Locaties</title>
-    <link rel="stylesheet" href="../stylesheet.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossorigin=""/>
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+            crossorigin=""></script>
+    <style>
+        #map { height: 600px; }
+    </style>
+    <title>Document</title>
 </head>
 <body>
 
-<div id="map"></div>
-<div id="map-type-selector">
-    <label for="map-type">Kies kaarttype: </label>
-    <select id="map-type" onchange="changeMapType(this.value)">
-        <option value="roadmap">Standaard</option>
-        <option value="satellite">Satelliet</option>
-    </select>
+
+
+
+
+<div id="map">
 </div>
-
 <script>
-    var map;
+    var map = L.map('map').setView([51.9500,4.5518], 13);
 
-    function initMap() {
-        // Coördinaten voor het centrum van Rotterdam
-        var rotterdam = { lat: 51.9225, lng: 4.47917 };
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-        // Maak een kaart aan met het centrum op Rotterdam
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: rotterdam,
-            zoom: 12,
-            mapTypeId: google.maps.MapTypeId.ROADMAP // Standaard kaarttype
-        });
-
-        // Voeg een marker toe voor het centrum van Rotterdam
-        var marker = new google.maps.Marker({ position: rotterdam, map: map, title: 'Rotterdam' });
-
-        // Voeg een click event listener toe aan de marker
-        marker.addListener('click', function () {
-            // Geocodeer de coördinaten naar een adres
-            var geocoder = new google.maps.Geocoder;
-            geocoder.geocode({ 'location': rotterdam }, function (results, status) {
-                if (status === 'OK') {
-                    if (results[0]) {
-                        alert('Adres: ' + results[0].formatted_address);
-                    } else {
-                        alert('Geen adres gevonden');
-                    }
-                } else {
-                    alert('Geocoderen mislukt vanwege: ' + status);
-                }
-            });
-        });
-    }
-
-    function changeMapType(type) {
-        if (type === 'roadmap') {
-            map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
-        } else if (type === 'satellite') {
-            map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
-        }
-    }
+    L.marker([51.9500, 4.5518]).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
 </script>
-
-<!-- Voeg de Google Maps JavaScript API toe zonder API-sleutel -->
-<script async defer src="https://maps.googleapis.com/maps/api/js?callback=initMap"></script>
-
 </body>
 </html>
