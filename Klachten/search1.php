8f4fb16271ba04ee10844d80d56b9b7d4ad5aa2f
Adonai10 <9014497@student.zadkine.nl>
2023-12-19 20:47:43 +0100

    search toegevoegd

---
+++ Klachten/search1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <form action="search2.php" method="post">
+        <label for ="id">ID:</label>
+        <input type="text" id="id" name="id">
+        <input type="submit">
+        <br>
+    </form>
+</div>
+
+</body>
+</html>
