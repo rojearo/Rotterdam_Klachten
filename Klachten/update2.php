565d3b75d2ca6b6abdd7bbd5533bbdc2e6084f83
Adonai10 <9014497@student.zadkine.nl>
2023-12-19 20:48:02 +0100

    update toegevoegd

---
+++ Klachten/update2.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+require "klachten.php";
+$id = $_POST["id"];
+$klachten1 = new klachten();
+$klachten1->search($id);
+
+// properties in variables
+$Soort = $klachten1->get_Soort();
+$Beschrijving = $klachten1->get_Beschrijving();
+?>
+
+<form action="update3.php" method="post">
+    <input type="hidden" name="id" value="<?php echo $id;?>"><br>
+    <input type="text" name="Soort" value="<?php echo $Soort;?>"><br>
+    <input type="text" name="Beschrijving" value="<?php echo $Beschrijving;?>"><br>
+    <input type="submit"><br>
+</form>
+</body>
+</html>
